7d1e48c10445b791f398e772b46ef056e5fec401
Robert Durica <r.durica@gmail.com>
2022-08-12 22:22:46 +0200

    fixed frontend

--- weather/tests/test_homepage.py
+++ weather/tests/test_homepage.py
@@ -1,3 +1,4 @@
+import os
 from http import HTTPStatus
 
 from django.test import TestCase
@@ -5,7 +6,15 @@ from django.urls import reverse
 
 
 class TestHomepageView(TestCase):
+    def setUp(self) -> None:
+        from django.conf import settings
+
+        settings.SECRET_KEY = "test"
+
     def test_homepage_available(self):
         response = self.client.get(reverse("weather:home"))
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(response, "Hi")
+
+    def test_homepage_no_data(self):
+        response = self.client.get(reverse("weather:home"))
+        self.assertContains(response, "Communication error. Please, try again later.")
